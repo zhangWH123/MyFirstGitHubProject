74ef7440fc19f617f238b2bf969a8e20c5aa82bf
张文浩 <70746847+zhangWH123@users.noreply.github.com>
2020-09-07 12:30:32 +0800

    Add files via upload

---
+++ Demo01GitHub.java
@@ -0,0 +1,7 @@
+package test9_7.demo01.GitHub;
+
+public class Demo01GitHub {
+    public static void main(String[] args) {
+        System.out.println("This is my first GitHub project。");
+    }
+}
